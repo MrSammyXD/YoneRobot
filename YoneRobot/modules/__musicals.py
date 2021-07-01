9d967fc1bb27a65f46a05c25df35de377982eaa1
jnckmax <85943108+jnckmax@users.noreply.github.com>
2021-07-01 07:16:31 +0530

    Update __musicals.py

--- YoneRobot/modules/__musicals.py
+++ YoneRobot/modules/__musicals.py
@@ -5,14 +5,12 @@ __help__ = """
 MUSIC !
 
 /video <i>query</i>: download video from youtube
-/deezer <i>query</i>: download from deezer
 /music <i>query</i>: download song from yt servers. (API BASED)
-/lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
-/glyrics <i> song name </i> : This plugin searches for song lyrics with song name and artist.
 
 VC PLAYER !
+
 Note ! 
 click the below hyper link to see commands Duh!
 
-[VC MUSIC YAY !](https://telegra.ph/%F0%93%84%82%E1%B4%8D%E1%B4%8F%CA%80%C9%AA-%E1%B4%8A%C9%AA%C9%B4-06-30).
+[VC MUSIC YAY !](https://telegra.ph/%F0%93%84%82%E1%B4%8D%E1%B4%8F%CA%80%C9%AA-%E1%B4%8A%C9%AA%C9%B4-06-30)
 """
