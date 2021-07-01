19d5c4bde5e9311f3c6eabfe6105f9cf869515f2
jnckmax <85943108+jnckmax@users.noreply.github.com>
2021-07-01 12:15:45 +0530

    Update __musicals.py

--- YoneRobot/modules/__musicals.py
+++ YoneRobot/modules/__musicals.py
@@ -7,10 +7,4 @@ MUSIC !
 /video <i>query</i>: download video from youtube
 /music <i>query</i>: download song from yt servers. (API BASED)
 
-VC PLAYER !
-
-Note ! 
-click the below hyper link to see commands Duh!
-
-[VC MUSIC YAY !](https://telegra.ph/%F0%93%84%82%E1%B4%8D%E1%B4%8F%CA%80%C9%AA-%E1%B4%8A%C9%AA%C9%B4-06-30)
 """
