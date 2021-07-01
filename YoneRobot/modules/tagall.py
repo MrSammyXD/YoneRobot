0e36f93955c632986706c7d30b1e299abad0f092
jnckmax <85943108+jnckmax@users.noreply.github.com>
2021-07-01 22:57:32 +0530

    Update tagall.py

--- YoneRobot/modules/tagall.py
+++ YoneRobot/modules/tagall.py
@@ -16,7 +16,7 @@ from YoneRobot import pbot
 @pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
 @admins_only
 async def tagall(client, message):
-    await message.reply("`Processing.....`")
+    await message.reply("`TAGGING ALL MEMBERS IN THE CHAT.....`")
     sh = get_text(message)
     if not sh:
         sh = "Hi!"
